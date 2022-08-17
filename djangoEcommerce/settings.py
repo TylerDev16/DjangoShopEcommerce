122151c39dd05d8d27a5f6893e9c3ab606c9a4d0
TylerDev16 <97607018+TylerDev16@users.noreply.github.com>
2022-08-17 05:05:26 -0400

    make it better

--- djangoEcommerce/settings.py
+++ djangoEcommerce/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'rosetta',
     'parler',
     'localflavor',
-    'sorl.thumbnail',
 ]
 
 LOCALE_PATHS = (
@@ -60,8 +59,8 @@ LOCALE_PATHS = (
 )
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -162,7 +161,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
